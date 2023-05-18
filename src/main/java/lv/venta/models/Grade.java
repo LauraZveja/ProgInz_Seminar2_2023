1accc2ee43471e3c091ef65213115a3204b0d1e2
LauraZveja <lauramaisele@hotmail.lv>
2023-05-18 10:51:34 +0300

    Update: Grade class

--- src/main/java/lv/venta/models/Grade.java
+++ src/main/java/lv/venta/models/Grade.java
@@ -1,5 +1,39 @@
 package lv.venta.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Table(name = "grade_table")
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
 public class Grade {
+	
+	@Setter(value = AccessLevel.NONE)
+	@Column(name = "Idg")
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long idg;
+	
+	
+	@Column(name = "GradeValue")
+	@Min(value = 0)
+	@Max(value = 10)
+	private int gradeValue;
+	
+	//TODO izveidot konstruktoru, kad būs sasaiste ar citām klasēm
 
 }

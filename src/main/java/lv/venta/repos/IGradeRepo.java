7d72e67d9f23bb3908e2cf3d562fe85a280d67d8
LauraZveja <lauramaisele@hotmail.lv>
2023-05-19 10:31:33 +0300

    Add: select functions in repo

--- src/main/java/lv/venta/repos/IGradeRepo.java
+++ src/main/java/lv/venta/repos/IGradeRepo.java
@@ -1,9 +1,18 @@
 package lv.venta.repos;
 
+import java.util.ArrayList;
+
 import org.springframework.data.repository.CrudRepository;
 
 import lv.venta.models.Grade;
 
 public interface IGradeRepo extends CrudRepository<Grade, Long>{
+	
+	//TODO atlasīt visu studentu atzīmes, ja zināms studentu id
+	ArrayList<Grade> findByStudentIds(long ids);
+	
+	//TODO atlasīt visa kursa atzīmes, ja ir zināms kursa nosaukums. 
+	//Uztaisīsies select, kas apvieno divas tabulas
+	ArrayList<Grade> findByCourseTitle(String title);
 
 }

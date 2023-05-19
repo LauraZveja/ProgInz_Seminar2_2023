f71af3d984320601b1fd98c5ca58c34baa828956
LauraZveja <lauramaisele@hotmail.lv>
2023-05-19 09:48:46 +0300

    Edit: fixed functions in main class

--- src/main/java/lv/venta/Seminar2DbApplication.java
+++ src/main/java/lv/venta/Seminar2DbApplication.java
@@ -1,5 +1,8 @@
 package lv.venta;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -30,20 +33,44 @@ public class Seminar2DbApplication {
 			@Override
 			public void run(String... args) throws Exception {
 				
+				ArrayList<Professor> professors = new ArrayList<>();
+				
 				Professor pr1 = new Professor("Karina", "Skirmante", Degree.mg);
 				Professor pr2 = new Professor("Martins", "Saulitis", Degree.mg);
 				profRepo.save(pr1);
 				profRepo.save(pr2);
+				
+				professors.add(pr1);
+				professors.add(pr2);
+				
+				
 				
 				Student st1 = new Student("Janis", "Berzins", Programme.BACHELOR);
 				Student st2 = new Student("Baiba", "Kalnina", Programme.BACHELOR);
 				stRepo.save(st1);
 				stRepo.save(st2);
 				
-				Course c1 = new Course("Java", 4, pr1);
-				Course c2 = new Course("Datubazes", 4, pr2);
+				
+				//TODO uztaisīt kursu, kam ir divi profesori piesaistīti
+				//TODO Uztaisīt gadījumu, kad viens profesors pasniedz divus kursus.
+				Course c1 = new Course("Java", 4, professors);
+				Course c2 = new Course("Datubazes", 4, new ArrayList<>(Arrays.asList(pr1, pr2))); //šādi arī var rakstīt, nevis kā c1 gadījumā
+				Course c3 = new Course("Datustrukturas", 4, new ArrayList<>(Arrays.asList(pr1)));
 				courRepo.save(c1);
 				courRepo.save(c2);
+				courRepo.save(c3);
+				
+				c1.addProfessor(pr1);
+				c1.addProfessor(pr2);
+				
+				c2.addProfessor(pr1);
+				c2.addProfessor(pr2);
+				
+				c3.addProfessor(pr1);
+				//tik gari jāraksta, lai pārliecinātos, ka many to many strādā
+				courRepo.save(c1);
+				courRepo.save(c2);
+				courRepo.save(c3);
 				
 				grrepo.save(new Grade(10, st1, c1)); //Jānis nopelnīja 10 JAVA
 				grrepo.save(new Grade(6, st1, c2)); //Jānis  nopelnīja 6 Datubāzēs

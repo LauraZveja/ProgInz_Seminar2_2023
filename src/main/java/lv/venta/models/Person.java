717769693eb386ce901d2b04d554d4a46d2600d1
LauraZveja <lauramaisele@hotmail.lv>
2023-05-19 10:10:12 +0300

    Add: new Person class

---
+++ src/main/java/lv/venta/models/Person.java
@@ -0,0 +1,42 @@
+package lv.venta.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+//Šī klase nebūs datubāzē, no šīs mantos
+@MappedSuperclass
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class Person {
+	
+	@Setter(value = AccessLevel.NONE)
+	@Column(name = "Idpe")
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long idpe;
+	
+	@Column(name = "Name")
+	@Size(min = 3, max =15)
+	@NotNull
+	@Pattern(regexp = "[A-ZĒŪĻĶ]{1}[a-zēūļķ]+", message = "Pirmajam burtam jābūt lielajam")
+	private String name;
+	
+	@Column(name = "Surname")
+	@Pattern(regexp = "[A-ZĒŪĻĶ]{1}[a-zēūļķ]+", message = "Pirmajam burtam jābūt lielajam")
+	@NotNull
+	private String surname;
+
+}
